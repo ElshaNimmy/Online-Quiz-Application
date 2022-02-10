6a87ac237fe3c9dba5e405d33ba18e688a3318d0
ElshaNimmy <shelmamathew24@gmail.com>
2022-02-10 12:39:22 +0530

    done

--- mpro/demoregister/src/main/java/com/example/demoregister/repository/SubjectRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demoregister.repository;
-
-import com.example.demoregister.entity.Subject;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubjectRepository extends JpaRepository<Subject,Long> {
-}
